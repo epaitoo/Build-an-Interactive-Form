15d1d995c1cd641fe1f03734e6102102bfe95224
Eugene Paitoo <eugenepaitoo@gmail.com>
2018-12-20 12:52:25 +0000

    worked T-shirt color options
    T-shirt color options are revealed based on the design selected.

--- js/app.js
+++ js/app.js
@@ -1,92 +1,89 @@
-
+// On page load, the cursor appears in the "Name" field, ready for a user to type
 const name = document.querySelector('#name');
 name.focus();
 
 const jobRoles = document.querySelector('#title');
+const design = document.querySelector('#design'); //select the design id
+const color = document.getElementById('color'); //gets the select element with id "#color"
 
-const design = document.querySelector('#design');
 
 
- 
+
+// job role text field appears when user selects "Other" from the Job Role menu 
 jobRoles.addEventListener('change', (e) => {
     const fieldSet = jobRoles.parentNode;
     const optionValue = jobRoles.value;
 
-        if (optionValue === 'other') {  
-            const label = document.createElement('label');
+        if (optionValue === 'other') {  //if the value of the jobrole === option with value 'other'
+            const label = document.createElement('label'); //then create a label
             const input = document.createElement('input');
             input.type = 'text';
             input.placeholder = 'Your Job Role';
             label.id = 'other-title';
             label.appendChild(input);
-            fieldSet.appendChild(label);
+            fieldSet.appendChild(label); 
         } else {
             const label = fieldSet.lastElementChild;
-            if (optionValue !== 'other') {
+            if (optionValue !== 'other') { 
                 label.style.display = 'none';
             }
         }
 });
 
+// hide and disable all from drop down
+for (let i = 0; i < color.children.length; i++) {
+    color.children[i].classList.add("none");
+}
 
-
-// design.addEventListener('change', (e) => {
-//     const shirtColor = document.getElementById("colors-js-puns");
-//     const color = document.getElementById('color');
-//     if (design.value == "js puns") {
-//         color.selectedIndex = "0";
-//         for(let i = 0; i < color.children.length; i++) {
-            
-//             if (color.children[i].value === "tomato" || color.children[i].value === "steelblue" || color.children[i].value === "dimgrey") {
-//                 color.children[i].style.display = "none";   
-//             } else {
-//                 color.children[i].style.display = ""; 
-                
-//             }     
-//         }  
-//     }
-
-//     if (design.value == "heart js") {
-//         color.selectedIndex = "0";
-//         for(let i = 0; i < color.children.length; i++) {
-//             if (color.children[i].value === "cornflowerblue" || color.children[i].value === "darkslategrey" || color.children[i].value === "gold") {
-//                 color.children[i].style.display = "none";     
-//             } else {
-//                 color.children[i].style.display = ""; 
-                
-//             }
-//         }
-//     } 
-// });
-
-design.addEventListener('change', (e) => {
-    const color = document.getElementById('color');
-
-    if (e.target.value == 'js puns') {
+// T-shirt color options are revealed based on the design selected.
+design.addEventListener('change', (e) => { 
+    
+    if (e.target.value === 'js puns') {
+        color.selectedIndex = 1; //shows the second option in the color select element
         for (let i = 0; i < color.children.length; i++) {
-            if (color.children[i].index <=2) {
+            if (color.children[i].index <=3) { //if the option index is less than or equal to 3
+                color.children[0].style.display = 'none';
                 color.children[i].classList.add('active');
+                color.children[i].classList.remove('none');              
             } else {
+                color.children[i].classList.contains('active');
                 color.children[i].classList.remove('active');
                 color.children[i].classList.add('none');
             }
         }
 
-    }
-    if (e.target.value == 'heart js') {
+    } else if (e.target.value === 'heart js') {
+        color.selectedIndex = 4; //shows the fifth option in the color select element
         for (let i = 0; i < color.children.length; i++) {
-            if (color.children[i].index >= 3) {
+            if (color.children[i].index >= 4) { //if the option index is greater than or equal to 4
                 color.children[i].classList.add('active');
+                color.children[i].classList.remove('none');
             } else {
+                color.children[i].classList.contains('active');
                 color.children[i].classList.remove('active');
-                color.children[i].classList.add('none'); 
+                color.children[i].classList.add('none');
             }
         }
-    }
-    
-    
+    } 
 });
  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
